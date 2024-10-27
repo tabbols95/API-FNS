aac851502b61c12dacc697afd892881960469e89
a.shilov <tabbols95@gmail.com>
2024-10-27 15:23:26 +0300

    Update request

--- src/fns/req.py
+++ src/fns/req.py
@@ -1,21 +1,67 @@
 import requests
 import json
-from typing import Callable
+from typing import Callable, Optional, List
+from dataclasses import dataclass
 
-def create_request(func: Callable, **kwargs):
 
-    url = f'https://api-fns.ru/api/{func.__name__}'
+@dataclass
+class AnswerRequest:
+    """Ответ по запросу"""
+
+    method: str
+    """Метод API ФНС"""
+
+    url: str = f'https://api-fns.ru/api'
+    """URL адрес"""
+
+    status_code: Optional[int] = None
+    """Код статуса ответа"""
+
+    status: Optional[str] = None
+    """Сообщение со статусом ответа"""
+
+    encoding: str = 'utf-8'
+    """Кодировка"""
+
+    result: List[dict] = None
+    """Ответ от сервиса"""
+
+    error_flag: bool = False
+    """Флаг ошибки"""
+
+    error_message: Optional[str] = None
+    """Сообщение об ошибке"""
+
+    def get_url(self) -> str:
+        full_url = self.url + '/' + self.method
+        return full_url
+
+
+def get_fns_data(func: Callable, **kwargs) -> AnswerRequest:
+
+    answer = AnswerRequest(method=func.__name__)
+    url = answer.get_url()
 
     try:
         response = requests.get(url=url, params=kwargs)
 
+        # Записываем ответ
+        answer.status_code = response.status_code
+        answer.status = response.reason
+        answer.encoding = response.encoding
+
         if response.status_code == 200:
             try:
                 json_data = response.json()
-                return json_data
+                answer.result = json_data.get('items', [])
             except requests.exceptions.JSONDecodeError as e:
-                raise f"JSON Decode Error: {e}\nResponse content: {response.text}"
-            except TypeError as e:
-                raise f"JSON Decode Error: {e}\nResponse content: {response.text}"
+                answer.error_flag = True
+                answer.error_message = f'JSONDecodeError: {e}.'
+        else:
+            answer.error_flag = True
+            answer.error_message = f'Error: {response.status_code}.'
     except requests.exceptions.RequestException as e:
-        raise f"An error occurred: {e}"
+        # Записываем ответ об ошибке
+        answer.error_flag = True
+        answer.error_message = f'RequestException: {e}.'
+    return answer
